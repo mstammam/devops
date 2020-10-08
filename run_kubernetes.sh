6be7ca037d3ef6c714d2c22b5ee071484c89f2f8
EC2 Default User <ec2-user@ip-172-31-27-28.us-east-2.compute.internal>
2020-10-08 18:45:34 +0000

    kubernetes files update

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="mtammam/udacity-devops"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-devops --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-devops 8000:80
