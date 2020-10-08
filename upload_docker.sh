b7177035d2652dcf7b5c35b43819de652d706e27
EC2 Default User <ec2-user@ip-172-31-27-28.us-east-2.compute.internal>
2020-10-08 16:18:13 +0000

    upload docker update

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="mtammam/udacity-devops"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login
+docker image tag udacity-devops $dockerpath
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
